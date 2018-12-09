df35c03d72e38c781ca5da396713a2dec0de74b4
ryan.zhuang <laozhuang727@hotmail.com>
2018-12-10 00:29:47 +0800

    my local test

--- models.py
+++ models.py
@@ -20,7 +20,31 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        
+        ## output size = (W-F)/S +1 = (224-5)/1 +1 = 220
+        # the output Tensor for one image, will have the dimensions: (32, 220, 220)
+        # after one pool layer, this becomes (32, 110, 110)
+       
         self.conv1 = nn.Conv2d(1, 32, 5)
+        
+        # maxpool layer
+        # pool with kernel_size=2, stride=2
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # second conv layer: 32 inputs, 64 outputs, 5x5 conv
+        ## output size = (W-F)/S +1 = (110-5)/1 +1 = 106
+        # the output tensor will have dimensions: (64, 106, 106)
+        # after another pool layer this becomes (64, 53, 53);  no need to rounded down
+        self.conv2 = nn.Conv2d(32, 16, 5)
+        
+        # 64 outputs * the 5*5 filtered/pooled map size
+        self.fc1 = nn.Linear(16*53*53, 272)
+        
+        self.fc1_drop = nn.Dropout(p=0.4)
+        
+        # finally, create 136 output channels (for the 136 classes)
+        self.fc2 = nn.Linear(272, 136)
+        
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -30,8 +54,19 @@ class Net(nn.Module):
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        # prep for linear layer
+        # this line of code is the equivalent of Flatten in Keras
+        x = x.view(x.size(0), -1)
         
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        
+        x = F.relu(self.fc2(x))
+
+
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
